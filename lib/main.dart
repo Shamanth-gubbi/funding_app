77b65b43d2e1cb154f84261046d3383d70590704
Vinayak N Magajikondi <88372964+VINAYAK-N-MAGAJIKONDI@users.noreply.github.com>
2023-03-03 17:17:22 +0530

    login page working fine for sponsor
    other pages are yet to be created

--- lib/main.dart
+++ lib/main.dart
@@ -38,10 +38,10 @@ class _MyLoadingScreenState extends State<MyLoadingScreen> {
       backgroundColor: Colors.blue,
       body: Center(
         child: Text(
-          'SCHOLARSHIP \n APP',
-          style: TextStyle(fontSize: 30 , color:Colors.white ,fontWeight:FontWeight.w800 )  ,textAlign:TextAlign.center,
-
-
+          'ABHUDAY',
+          style: TextStyle(
+              fontSize: 30, color: Colors.white, fontWeight: FontWeight.w800),
+          textAlign: TextAlign.center,
         ),
       ),
     );
@@ -53,19 +53,27 @@ class MyLoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue,
-
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            SizedBox(height: 40 ,
-                child: Text('Are you ' , textAlign: TextAlign.center,style: TextStyle(fontSize: 30 , color:Colors.white , fontWeight:FontWeight.w600),)),
-
+            SizedBox(
+                height: 40,
+                child: Text(
+                  'Are you ',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600),
+                )),
             ElevatedButton(
               style: ButtonStyle(
                 padding: MaterialStateProperty.all<EdgeInsets>(
                   EdgeInsets.symmetric(vertical: 16.0, horizontal: 60.0),
-                ),backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF8ED0FF)),
+                ),
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Color(0xFF8ED0FF)),
                 shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                   RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(100),
@@ -79,27 +87,38 @@ class MyLoginPage extends StatelessWidget {
                   children: [
                     Text(
                       'A Student',
-                      style: TextStyle(fontSize: 30 , color:Colors.white , fontWeight:FontWeight.w600),
+                      style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600),
                     ),
                     Icon(Icons.person),
                   ],
                 ),
               ),
               onPressed: () {
-                Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (context) => OptionScreenStudent()));
+                Navigator.of(context).pushReplacement(MaterialPageRoute(
+                    builder: (context) => OptionScreenStudent()));
                 // TODO: Add signup logic
               },
-
-
             ),
-            SizedBox(height: 40 ,
-            child: Text('OR ' , textAlign: TextAlign.center,style: TextStyle(fontSize: 30 , color:Colors.white , fontWeight:FontWeight.w600),)),
+            SizedBox(
+                height: 40,
+                child: Text(
+                  'OR ',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600),
+                )),
             ElevatedButton(
               style: ButtonStyle(
                 padding: MaterialStateProperty.all<EdgeInsets>(
                   EdgeInsets.symmetric(vertical: 16.0, horizontal: 60.0),
-                ),backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF8ED0FF)),
+                ),
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Color(0xFF8ED0FF)),
                 shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                   RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(100),
@@ -112,21 +131,22 @@ class MyLoginPage extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text(
-                      'A Sponsor',
-                      style: TextStyle(fontSize: 30 , color:Colors.white , fontWeight:FontWeight.w600),
+                      'A Trust / Sponsor',
+                      style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600),
                     ),
                     Icon(Icons.attach_money_rounded),
                   ],
                 ),
               ),
               onPressed: () {
-
-                Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (context) => OptionScreenSponsor())
-                // TODO: Add signup logic
-                );},
-
-
+                Navigator.of(context).pushReplacement(MaterialPageRoute(
+                        builder: (context) => OptionScreenSponsor())
+                    // TODO: Add signup logic
+                    );
+              },
             ),
           ],
         ),
@@ -135,7 +155,6 @@ class MyLoginPage extends StatelessWidget {
   }
 }
 
-
 /////////////////////////////////////
 
 class OptionScreenSponsor extends StatelessWidget {
@@ -143,19 +162,27 @@ class OptionScreenSponsor extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue,
-
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            SizedBox(height: 40 ,
-                child: Text('Welcome Sponsor! ' , textAlign: TextAlign.center,style: TextStyle(fontSize: 30 , color:Colors.white , fontWeight:FontWeight.w600),)),
-
+            SizedBox(
+                height: 40,
+                child: Text(
+                  'Welcome Sponsor! ',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600),
+                )),
             ElevatedButton(
               style: ButtonStyle(
                 padding: MaterialStateProperty.all<EdgeInsets>(
                   EdgeInsets.symmetric(vertical: 16.0, horizontal: 60.0),
-                ),backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF8ED0FF)),
+                ),
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Color(0xFF8ED0FF)),
                 shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                   RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(100),
@@ -169,24 +196,31 @@ class OptionScreenSponsor extends StatelessWidget {
                   children: [
                     Text(
                       'Login ',
-                      style: TextStyle(fontSize: 30 , color:Colors.white , fontWeight:FontWeight.w600),
+                      style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600),
                     ),
                     Icon(Icons.arrow_forward),
                   ],
                 ),
               ),
               onPressed: () {
+                Navigator.of(context).pushReplacement(MaterialPageRoute(
+                    builder: (context) => LoginScreen())
+                  // TODO: Add signup logic
+                );
                 // TODO: Add signup logic
               },
-
-
             ),
-            SizedBox(height: 40 ),
+            SizedBox(height: 40),
             ElevatedButton(
               style: ButtonStyle(
                 padding: MaterialStateProperty.all<EdgeInsets>(
                   EdgeInsets.symmetric(vertical: 16.0, horizontal: 60.0),
-                ),backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF8ED0FF)),
+                ),
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Color(0xFF8ED0FF)),
                 shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                   RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(100),
@@ -200,19 +234,18 @@ class OptionScreenSponsor extends StatelessWidget {
                   children: [
                     Text(
                       'Sign Up',
-                      style: TextStyle(fontSize: 30 , color:Colors.white , fontWeight:FontWeight.w600),
+                      style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600),
                     ),
                     Icon(Icons.arrow_forward),
                   ],
                 ),
               ),
               onPressed: () {
-
-
-                    // TODO: Add signup logic
-                },
-
-
+                // TODO: Add signup logic
+              },
             ),
           ],
         ),
@@ -227,19 +260,27 @@ class OptionScreenStudent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue,
-
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            SizedBox(height: 40 ,
-                child: Text('Welcome Sponsor! ' , textAlign: TextAlign.center,style: TextStyle(fontSize: 30 , color:Colors.white , fontWeight:FontWeight.w600),)),
-
+            SizedBox(
+                height: 40,
+                child: Text(
+                  'Welcome Student! ',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600),
+                )),
             ElevatedButton(
               style: ButtonStyle(
                 padding: MaterialStateProperty.all<EdgeInsets>(
                   EdgeInsets.symmetric(vertical: 16.0, horizontal: 60.0),
-                ),backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF8ED0FF)),
+                ),
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Color(0xFF8ED0FF)),
                 shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                   RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(100),
@@ -253,7 +294,10 @@ class OptionScreenStudent extends StatelessWidget {
                   children: [
                     Text(
                       'Login ',
-                      style: TextStyle(fontSize: 30 , color:Colors.white , fontWeight:FontWeight.w600),
+                      style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600),
                     ),
                     Icon(Icons.arrow_forward),
                   ],
@@ -262,15 +306,15 @@ class OptionScreenStudent extends StatelessWidget {
               onPressed: () {
                 // TODO: Add signup logic
               },
-
-
             ),
-            SizedBox(height: 40 ),
+            SizedBox(height: 40),
             ElevatedButton(
               style: ButtonStyle(
                 padding: MaterialStateProperty.all<EdgeInsets>(
                   EdgeInsets.symmetric(vertical: 16.0, horizontal: 60.0),
-                ),backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF8ED0FF)),
+                ),
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Color(0xFF8ED0FF)),
                 shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                   RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(100),
@@ -284,19 +328,18 @@ class OptionScreenStudent extends StatelessWidget {
                   children: [
                     Text(
                       'Sign Up',
-                      style: TextStyle(fontSize: 30 , color:Colors.white , fontWeight:FontWeight.w600),
+                      style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600),
                     ),
                     Icon(Icons.arrow_forward),
                   ],
                 ),
               ),
               onPressed: () {
-
-
                 // TODO: Add signup logic
               },
-
-
             ),
           ],
         ),
@@ -306,3 +349,171 @@ class OptionScreenStudent extends StatelessWidget {
 }
 
 
+/////////////
+
+////////////
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+                height: 40,
+                child: Text(
+                  'Log in for sponsors ',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w600),
+                )),
+        SizedBox(
+            width: MediaQuery.of(context).size.width * 0.9,
+            child: TextFormField(
+              keyboardType: TextInputType.emailAddress,
+
+
+              decoration: InputDecoration(
+                  labelText: 'Email',
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  hintStyle: TextStyle(fontSize: 14),
+                  hintText: 'Email',
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(100), borderSide: BorderSide.none),
+                  contentPadding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 50.0)
+              ),
+
+
+            )
+
+
+
+        ),
+
+
+            SizedBox(height: 30,),
+
+
+            SizedBox(
+                width: MediaQuery.of(context).size.width * 0.9,
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      labelText: 'Password',
+
+                      filled: true,
+                      fillColor: Colors.grey[200],
+                      hintStyle: TextStyle(fontSize: 14),
+                      hintText: 'Password',
+                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(100), borderSide: BorderSide.none),
+                      contentPadding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 50.0)
+                  ),
+
+
+                )
+
+
+
+            ),
+
+
+
+
+
+
+            TextButton(
+                child:
+            Text('Forgot password ' ,textAlign: TextAlign.left,style: TextStyle( color: Colors.black26),),
+              onPressed: () {  },),
+
+
+            ElevatedButton(
+              style: ButtonStyle(
+                padding: MaterialStateProperty.all<EdgeInsets>(
+                  EdgeInsets.symmetric(vertical: 16.0, horizontal: 60.0),
+                ),
+                backgroundColor:
+                MaterialStateProperty.all<Color>(Color(0xFF42B0FF)),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                ),
+              ),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.6,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Log in ',
+                      style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600))
+                  ],
+                ),
+              ),
+              onPressed: () {
+                // TODO: Add signup logic
+              },
+            ),
+
+
+
+
+
+            SizedBox(height: 60 ,
+            child: Center(
+                child: Text('OR' , style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black45,
+                    fontWeight: FontWeight.w600
+
+                ) )
+            )
+            ,),
+            ElevatedButton(
+              style: ButtonStyle(
+                padding: MaterialStateProperty.all<EdgeInsets>(
+                  EdgeInsets.symmetric(vertical: 16.0, horizontal: 60.0),
+                ),
+                backgroundColor:
+                MaterialStateProperty.all<Color>(Color(0xF3F4F7)),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                ),
+              ),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.6,
+                child: Row(
+
+                  children: [
+
+                    Text(
+                      'Google ',
+                      style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black54,
+                          fontWeight: FontWeight.w600),
+                    ),
+
+                  ],
+                ),
+              ),
+              onPressed: () {
+                // TODO: Add signup logic
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
